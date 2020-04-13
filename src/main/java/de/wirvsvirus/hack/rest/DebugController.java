57aef4af23e85dc19fc5d9aa019a72383340faf8
cc-cpo <codecave@gmx.net>
2020-04-13 15:17:25 +0200

    protect debug endpoint

--- src/main/java/de/wirvsvirus/hack/rest/DebugController.java
+++ src/main/java/de/wirvsvirus/hack/rest/DebugController.java
@@ -1,13 +1,17 @@
 package de.wirvsvirus.hack.rest;
 
+import com.google.common.base.Preconditions;
+import com.google.common.hash.Hashing;
 import de.wirvsvirus.hack.mock.MockFactory;
 import de.wirvsvirus.hack.model.Group;
 import de.wirvsvirus.hack.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 
 @RestController
@@ -16,13 +20,25 @@ import java.util.Collection;
 public class DebugController {
 
     @GetMapping("/users")
-    public Collection<User> getAllUsers() {
+    public Collection<User> getAllUsers(
+            @RequestHeader("X-FAM-Debug") String debugCode
+    ) {
+        checkDebugCode(debugCode);
         return MockFactory.allUsers.values();
     }
 
     @GetMapping("/groups")
-    public Collection<Group> getAllGroups() {
+    public Collection<Group> getAllGroups(@RequestHeader("X-FAM-Debug") String debugCode) {
+        checkDebugCode(debugCode);
         return MockFactory.allGroups.values();
+    }
+
+    private void checkDebugCode(final String debugCode) {
+        final String hashed = Hashing.sha256().hashString(debugCode, StandardCharsets.ISO_8859_1).toString();
+        final String expected = "ff43ee88b4ef1c750519c6d681dc9992d990f6e852021b48d8a5faf182af1f27";
+        if (!expected.equals(hashed)) {
+            throw new IllegalArgumentException("Secret debug hash is wrong - ask Stefan");
+        }
     }
 
 }
