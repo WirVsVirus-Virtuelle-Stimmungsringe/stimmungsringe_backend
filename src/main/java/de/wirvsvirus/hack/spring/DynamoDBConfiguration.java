2f752fbde80f2a586f87c5f61a697169d34a43f5
cc-cpo <codecave@gmx.net>
2020-04-26 19:41:22 +0200

    add support for AWS dynamodb settings

--- src/main/java/de/wirvsvirus/hack/spring/DynamoDBConfiguration.java
+++ src/main/java/de/wirvsvirus/hack/spring/DynamoDBConfiguration.java
@@ -1,9 +1,14 @@
 package de.wirvsvirus.hack.spring;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +19,7 @@ import org.springframework.context.annotation.Profile;
 @Profile("dynamodb")
 public class DynamoDBConfiguration {
 
-    @Value("${amazon.dynamodb.endpoint}")
+    @Value("${amazon.dynamodb.endpoint:}")
     private String amazonDynamoDBEndpoint;
 
     @Value("${amazon.aws.accesskey}")
@@ -25,11 +30,19 @@ public class DynamoDBConfiguration {
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        AmazonDynamoDB amazonDynamoDB
-                = new AmazonDynamoDBClient(amazonAWSCredentials());
+        final AmazonDynamoDB amazonDynamoDB;
 
-        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
-            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+        if (StringUtils.isNotBlank(amazonDynamoDBEndpoint)) {
+            amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(amazonAWSCredentials()))
+                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                            amazonDynamoDBEndpoint, Regions.EU_WEST_1.getName()))
+                    .build();
+        } else {
+             amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(amazonAWSCredentials()))
+                    .withRegion(Regions.EU_WEST_1)
+                    .build();
         }
 
         return amazonDynamoDB;
