3204794a160358d6a198cee674a9ffb383123758
cc-cpo <codecave@gmx.net>
2020-07-12 20:31:00 +0200

    log time spent in rest endpoints

---
+++ src/main/java/de/wirvsvirus/hack/spring/RestEndpointTimingsStatsFilter.java
@@ -0,0 +1,47 @@
+package de.wirvsvirus.hack.spring;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@WebFilter("/stimmungsring/*")
+public class RestEndpointTimingsStatsFilter implements Filter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestEndpointTimingsStatsFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // empty
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        final StopWatch stopWatch = StopWatch.createStarted();
+        try {
+            chain.doFilter(req, resp);
+        } finally {
+            LOGGER.debug("Time spent in endpoint {}: {} ms", ((HttpServletRequest) req).getRequestURI(),
+                    BigDecimal.valueOf(stopWatch.getTime(TimeUnit.MICROSECONDS), 3).round(new MathContext(1)));
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // empty
+    }
+}
