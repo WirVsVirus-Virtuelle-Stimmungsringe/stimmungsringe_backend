7e69ff508aab019b9a94d59cf4f1797f222ea1a8
cc-cpo <codecave@gmx.net>
2020-08-17 21:00:39 +0200

    use lombok

--- src/main/java/de/wirvsvirus/hack/model/Message.java
+++ src/main/java/de/wirvsvirus/hack/model/Message.java
@@ -1,10 +1,12 @@
 package de.wirvsvirus.hack.model;
 
+import lombok.Data;
 import lombok.ToString;
 
 import java.time.Instant;
 import java.util.UUID;
 
+@Data
 @ToString
 public class Message {
 
@@ -15,51 +17,4 @@ public class Message {
     private UUID recipientUserId;
     private String text;
 
-    public UUID getGroupId() {
-        return groupId;
-    }
-
-    public void setGroupId(final UUID groupId) {
-        this.groupId = groupId;
-    }
-
-    public UUID getMessageId() {
-        return messageId;
-    }
-
-    public void setMessageId(final UUID messageId) {
-        this.messageId = messageId;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(final Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public UUID getSenderUserId() {
-        return senderUserId;
-    }
-
-    public void setSenderUserId(final UUID senderUserId) {
-        this.senderUserId = senderUserId;
-    }
-
-    public UUID getRecipientUserId() {
-        return recipientUserId;
-    }
-
-    public void setRecipientUserId(final UUID recipientUserId) {
-        this.recipientUserId = recipientUserId;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(final String text) {
-        this.text = text;
-    }
 }
