2eb5c859e2663a86a0905a66db669c3c2ca3b407
cc-cpo <codecave@gmx.net>
2021-04-19 20:38:09 +0200

    make UserInterceptor a spring bean

--- src/main/java/de/wirvsvirus/hack/spring/WebMvcConfig.java
+++ src/main/java/de/wirvsvirus/hack/spring/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package de.wirvsvirus.hack.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,8 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Bean
+    UserInterceptor userInterceptor() {
+        return new UserInterceptor();
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UserInterceptor());
+        registry.addInterceptor(userInterceptor());
     }
 }
