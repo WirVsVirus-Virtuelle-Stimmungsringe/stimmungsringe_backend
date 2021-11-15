0be48656f262b784fb93e8ccaa051ee7e1d4ff0e
cc-cpo <codecave@gmx.net>
2021-11-15 20:35:07 +0100

    fix migration for lastSignin

--- src/main/java/de/wirvsvirus/hack/repository/microstream/MigrationMetadata.java
+++ src/main/java/de/wirvsvirus/hack/repository/microstream/MigrationMetadata.java
@@ -21,6 +21,9 @@ public class MigrationMetadata implements MicrostreamObject {
 
   boolean staleMessagesDeleted;
 
+  @Deprecated
   boolean lastSigninInitialized2;
+
+  boolean lastSigninInitialized3;
 
 }
