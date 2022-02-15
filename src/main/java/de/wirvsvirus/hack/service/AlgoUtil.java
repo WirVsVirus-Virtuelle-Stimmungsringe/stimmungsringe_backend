537438900e0970ebe02b22ee69693f05a7fb512d
Dimitri B <dimitri.benin@gmail.com>
2022-02-15 20:50:15 +0100

    Use a simple array lookup to check for fibonacci number

--- src/main/java/de/wirvsvirus/hack/service/AlgoUtil.java
+++ src/main/java/de/wirvsvirus/hack/service/AlgoUtil.java
@@ -1,8 +1,14 @@
 package de.wirvsvirus.hack.service;
 
+import com.google.common.base.Preconditions;
+import java.util.Arrays;
 import java.util.Random;
 
 public class AlgoUtil {
+  private static long fibonacci30[] = {
+    0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946,
+    17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229
+  };
 
   /** must not start with 0 */
   public static String generateGroupCode() {
@@ -10,14 +16,12 @@ public class AlgoUtil {
     return String.valueOf(code);
   }
 
-  /** https://stackoverflow.com/questions/2432669/test-if-a-number-is-fibonacci */
   static boolean isFibonacciNumber(final long numberToTest) {
-    final double root5 = Math.sqrt(5);
-    final double phi = (1 + root5) / 2;
-
-    final long fibIndex = (long) Math.floor(Math.log(numberToTest * root5) / Math.log(phi) + 0.5);
-    final long res = (long) Math.floor(Math.pow(phi, fibIndex) / root5 + 0.5);
-
-    return (res == numberToTest);
+    Preconditions.checkArgument(
+        numberToTest <= fibonacci30[fibonacci30.length - 1],
+        String.format(
+            "This method is only defined for numbers <= %d, got: %d",
+            fibonacci30[fibonacci30.length - 1], numberToTest));
+    return Arrays.stream(fibonacci30).filter(fib -> fib == numberToTest).count() > 0;
   }
 }
